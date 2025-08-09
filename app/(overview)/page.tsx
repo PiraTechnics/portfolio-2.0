c822436e743c6d3a1d9b5ebadc9076a0127ba19f
PiraTechnics <devin1younge@gmail.com>
2025-08-08 20:23:57 -0700

    Under Construction Message

--- app/(overview)/page.tsx
+++ app/(overview)/page.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import { inter } from "../fonts";
 
-const desc =
-	"Devin Younge (He/Him) is a writer specializing in Technology, Tabletop RPGs, and Fantasy Fiction. He has also worked in journalism, entertainment, and software development.";
+/* const desc =
+  "Devin Younge (He/Him) is a writer specializing in Technology, Tabletop RPGs, and Fantasy Fiction. He has also worked in journalism, entertainment, and software development.";
+ */
+
+const desc = "Website Under Construction, Check back soon!";
 
 export default function Home() {
-	return (
-		<div className=" flex flex-col items-center justify-center gap-4 mx-2">
-			<Image
+  return (
+    <div className=" flex flex-col items-center justify-center gap-4 mx-2">
+      {/*
+            <Image
 				src="/profile.jpg"
 				alt="Vercel Logo"
 				//className="dark:invert"
@@ -20,9 +24,9 @@ export default function Home() {
 				className={`${inter.className} text-center text-5xl font-semibold bg-gradient-to-br from-slate-300 to-white text-transparent bg-clip-text inline-block p-1`}
 			>
 				Devin Younge, Writer
-			</h1>
+			</h1> */}
 
-			<div className="text-lg text-center">{desc}</div>
-		</div>
-	);
+      <div className="text-lg text-center">{desc}</div>
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,27 +6,27 @@ import { ExternalNav } from "./ui/components/externalNav";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-	title: "Devin Younge",
-	description: "Devin Younge - Author & TTRPG Content Creator",
+  title: "Devin Younge",
+  description: "Devin Younge - Author & TTRPG Content Creator",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={`${quicksand.className} bg-gradient-to-br from-bg-1 to-bg-2 text-slate-100 flex justify-center`}
-			>
-				<div className="mx-auto max-w-3xl min-h-screen flex flex-col items-center justify-center gap-10 sm:gap-6 mt-6 mb-12">
-					{children}
-					<NavButtons />
-					<ExternalNav />
-				</div>
-				<Analytics />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`${quicksand.className} bg-gradient-to-br from-bg-1 to-bg-2 text-slate-100 flex justify-center`}
+      >
+        <div className="mx-auto max-w-3xl min-h-screen flex flex-col items-center justify-center gap-10 sm:gap-6 mt-6 mb-12">
+          {children}
+          {/* <NavButtons />
+					<ExternalNav /> */}
+        </div>
+        <Analytics />
+      </body>
+    </html>
+  );
 }
