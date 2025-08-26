6c88a2a6f7920c7ddbc1ccaf05fd52fa9a84afd6
PiraTechnics <devin1younge@gmail.com>
2025-08-25 17:50:47 -0700

    back to previous state

--- app/(overview)/page.tsx
+++ app/(overview)/page.tsx
@@ -1,30 +1,28 @@
 import Image from "next/image";
 import { inter } from "../fonts";
 
-/* const desc =
+const desc =
   "Devin Younge (He/Him) is a writer specializing in Technology, Tabletop RPGs, and Fantasy Fiction. He has also worked in journalism, entertainment, and software development.";
- */
 
-const desc = "Website under construction, check back soon!";
+//const desc = "Website under construction, check back soon!";
 
 export default function Home() {
   return (
     <div className=" flex flex-col items-center justify-center gap-4 mx-2">
-      {/*
-            <Image
-				src="/profile.jpg"
-				alt="Vercel Logo"
-				//className="dark:invert"
-				className="rounded-full"
-				width={150}
-				height={150}
-				priority
-			/>
-			<h1
-				className={`${inter.className} text-center text-5xl font-semibold bg-gradient-to-br from-slate-300 to-white text-transparent bg-clip-text inline-block p-1`}
-			>
-				Devin Younge, Writer
-			</h1> */}
+      <Image
+        src="/profile.jpg"
+        alt="Vercel Logo"
+        //className="dark:invert"
+        className="rounded-full"
+        width={150}
+        height={150}
+        priority
+      />
+      <h1
+        className={`${inter.className} text-center text-5xl font-semibold bg-gradient-to-br from-slate-300 to-white text-transparent bg-clip-text inline-block p-1`}
+      >
+        Devin Younge, Writer
+      </h1>
 
       <div className="text-lg text-center">{desc}</div>
     </div>
